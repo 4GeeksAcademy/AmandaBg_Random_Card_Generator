9d2be2eaf281b633f91f53fcdb3eee03f49e610d
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-06-28 23:10:06 +0000

    version 01.Generador de Cartas Aleatorias

--- src/app.js
+++ src/app.js
@@ -4,8 +4,50 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { eventListeners } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var boton = document.getElementById("button");
+  boton.addEventListener("click", function() {
+    clearCard();
+    newcard();
+  });
+};
+
+function newcard() {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML =
+    "<span>" + generateRandomNumber() + "</span>";
+}
+function clearCard() {
+  document
+    .querySelector(".card")
+    .classList.remove("spade", "club", "heart", "diamond");
+  document.querySelector(".card").innerHTML = "<span></span>";
+}
+
+let generateRandomSuit = () => {
+  let suit = ["spade", "club", "heart", "diamond"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
 };
